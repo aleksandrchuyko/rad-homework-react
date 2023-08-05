0b6813f75b086645f1a2a507494ddfe2a39423c5
aleksandrchuyko <chao.mess@gmail.com>
2023-08-05 21:06:40 +0300

    Update NoteTable.tsx

--- src/components/NoteTable/NoteTable.tsx
+++ src/components/NoteTable/NoteTable.tsx
@@ -1,6 +1,5 @@
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
-
 import { nanoid } from '@reduxjs/toolkit';
 
 import { getNotes } from '../../redux/selectors';
